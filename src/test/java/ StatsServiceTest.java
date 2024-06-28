2ef33245f0f6643e78737351eb0ec4bac07d657b
Alex5453 <130847438+Alex5453@users.noreply.github.com>
2024-06-28 16:30:46 +0300

    Update  StatsServiceTest.java

--- src/test/java/ StatsServiceTest.java
+++ src/test/java/ StatsServiceTest.java
@@ -61,7 +61,14 @@ public class StatsServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
+   @Test
     void shouldCountMonthsAboveAverage() {
         StatsService service = new StatsService();
-        long[] sales = {8, 15, 13, 15,
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.countMonthsAboveAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+}
